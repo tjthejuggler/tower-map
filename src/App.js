4ed5f398ae606c78859f9a3237deb76081861bda
tjthejuggler <tjthejuggler@gmail.com>
2024-10-11 17:58:17 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+   import { MapContainer, TileLayer } from 'react-leaflet';
+   import 'leaflet/dist/leaflet.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   function App() {
+     return (
+       <div className="App">
+         <h1>Tower Visibility Calculator</h1>
+         <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '400px', width: '100%' }}>
+           <TileLayer
+             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+           />
+         </MapContainer>
+       </div>
+     );
+   }
 
-export default App;
+   export default App;
