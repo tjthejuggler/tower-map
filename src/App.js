ff962be8dd9131fe018cac3282b8b1100c254632
tjthejuggler <tjthejuggler@gmail.com>
2024-10-11 18:16:11 -0400

    map added back in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Suspense, lazy } from 'react';
-
-const MapComponent = lazy(() => import('./MapComponent'));
+import React from 'react';
+import MapComponent from './MapComponent';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Tower Visibility Calculator</h1>
-      <Suspense fallback={<div>Loading map...</div>}>
-        <MapComponent />
-      </Suspense>
+      <MapComponent />
     </div>
   );
 }
